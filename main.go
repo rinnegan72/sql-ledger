7fc8791c328cf61bb7d27658f5b5c290adffca82
Sriram Ramesh <sriram@vananam.com>
2025-12-21 12:11:27 +0530

    fully fixed

--- main.go
+++ main.go
@@ -26,9 +26,10 @@ func main() {
 	sql_db_host := os.Getenv("DATABASE_HOST")
 	sql_db_user := os.Getenv("DATABASE_USER")
 	sql_db_password := os.Getenv("DATABASE_PASSWORD")
+	sql_db_port := os.Getenv("DATABASE_PORT")
 	sql_db_name := os.Getenv("DATABASE_NAME")
 	sql_ssl_mode := os.Getenv("DATABASE_SSL_MODE")
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/ssl_mode=?%s", sql_db_user, sql_db_password, sql_db_host, sql_db_name, sql_ssl_mode)
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?tls=%s", sql_db_user, sql_db_password, sql_db_host, sql_db_port, sql_db_name, sql_ssl_mode)
 	// dsn form for mysql: username:password@protocol(address)/dbname?param=value
 	var err error
 	db, err = sql.Open("mysql", connStr)
@@ -48,27 +49,30 @@ func updateByID(c *gin.Context) {
 		return
 	}
 	if action == "deposit" || action == "withdraw" {
-		fmt.Println("performing ", action, " transaction for userid ", id, " amount ", user.Amount)
 		tx, err := db.Begin()
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Something went wrong"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Something went wrong with connecting to the DB"})
+			fmt.Println(err)
 			return
 		}
+		fmt.Println("performing ", action, " transaction for userid ", id, " amount ", user.Amount)
 		var account_balance int
-		err = tx.QueryRow("SELECT balance FROM accounts WHERE id = $1 FOR UPDATE", id).Scan(&account_balance)
+		err = tx.QueryRow("SELECT balance FROM accounts WHERE id = ? FOR UPDATE", id).Scan(&account_balance)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Something went wrong"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to start transaction"})
+			fmt.Println(err)
 			return
 		}
 		transaction_amount := user.Amount
 		if action == "withdraw" {
 			if transaction_amount > account_balance {
 				c.JSON(http.StatusBadRequest, gin.H{"error": "Insufficient balance"})
+				return
 			}
 			transaction_amount = transaction_amount * -1
 		}
 		account_balance = account_balance + transaction_amount
-		_, err = tx.Exec("UPDATE accounts SET balance = $1 WHERE id = $2", account_balance, id)
+		_, err = tx.Exec("UPDATE accounts SET balance = ? WHERE id = ?", account_balance, id)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Update failed"})
 			return
@@ -82,7 +86,4 @@ func updateByID(c *gin.Context) {
 		fmt.Println("Invalid Action")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Action"})
 	}
-}
-func createUser(c *gin.Context) {
-
 }
